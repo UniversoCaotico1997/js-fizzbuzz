300d17f4205036e8a4de05063f4272f5ec65c78f
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-10 16:52:48 +0200

    ciclo dei numeri con Fizz / Buzz / FizzBuzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //Consegna:
 //Scrivi un programma che stampi in console i numeri da 1 a 100,
-//ma che per i multipli di 3 stampi “Fizz” al posto del numero
+//per i multipli di 3 stampi “Fizz” al posto del numero
 //e per i multipli di 5 stampi “Buzz”.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 //Prima di partire a scrivere codice poniamoci qualche domanda:
@@ -20,19 +20,42 @@
 // let / const
 // for / loop
 // if / else 
-// Math.random / Math.ceil 
 
 
 //Scrivi un programma che stampi in console i numeri da 1 a 100
- 
+
+
 // Utlizzare for per il ciclo dei numeri 
+
 
 for(let i = 0; i <= 100; i++){
     console.log(i);
     // stampo dei numeri da 1 a 100 
+
+    // condizioni se è multiplo di 3
+    if (i % 3 === 0){
+    // stampo fizz
+    console.log(`Fizz`);
     
-
-
-
+    //condizioni se è multiplo di 5  
+    } else if (i % 5 === 0){
+    // stampo buzz
+    console.log(`Buzz`);
+    
+    // condizione se sono multipli di 3 e 5 
+    } else if (i % 3 === 0 && i % 5 === 0){
+    // stampo FizzBuzz
+    console.log(`FizzBuzz`);  
+    
+    // altrimenti 
+    } else{
+    // stampo in console i numeri normalmente 
+    console.log(i);
+    }
 }
+
+
+
+
+
 
